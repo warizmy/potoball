078901a66eb221463b0c1f597ece3af0c39c962f
unknown <abidalkhaw@gmail.com>
2024-01-03 11:38:30 +0800

    finalize responsive for laptop

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,4 +10,31 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+  const goToTopBtn = document.getElementById('on-top-btn');
+  let prevScrollHeight = window.scrollY;
+
+  window.addEventListener('scroll', () => {
+    const scrollHeight = window.scrollY;
+    const scrollThreshold = 1200;
+
+    if (scrollHeight > scrollThreshold) {
+      goToTopBtn.classList.add('scrolled');
+    } else {
+      goToTopBtn.classList.remove('scrolled');
+    }
+
+    if (scrollHeight < prevScrollHeight) {
+      goToTopBtn.classList.remove('scrolled');
+    }
+
+    prevScrollHeight = scrollHeight;
+  });
+
+  goToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
 });
